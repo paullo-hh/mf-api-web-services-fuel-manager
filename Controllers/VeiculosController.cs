c06a0b0e8afa95be300da0b6de0500f6d5ed3e88
Paulo H. Sousa <contacthenry96@gmail.com>
2025-08-20 23:17:35 -0300

    feat: implementa recurso para buscar todos os veiculos

---
+++ Controllers/VeiculosController.cs
@@ -0,0 +1,38 @@
+﻿using mf_api_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_api_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Veiculos
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            //return await _context.Veiculos.Include(v => v.Consumos).ToListAsync();
+            return Ok(await _context.Veiculos.ToListAsync());
+        }
+
+        // GET: api/Veiculos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Veiculo>> GetVeiculo(int id)
+        {
+            var veiculo = await _context.Veiculos.Include(v => v.Consumos).FirstOrDefaultAsync(v => v.Id == id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+            return veiculo;
+        }
+    }
+}
