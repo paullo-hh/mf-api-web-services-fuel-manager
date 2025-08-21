fbe618c961389e65146dd5330af9165458da1cf3
Paulo H. Sousa <contacthenry96@gmail.com>
2025-08-20 22:31:36 -0300

    feat: mapeia e configura as entidades Veiculo e Consumo

---
+++ Models/Veiculo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_api_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public String Nome { get; set; }
+
+        [Required]
+        public String Modelo { get; set; }
+
+        [Required]
+        public string Placa { get; set; }
+        
+        [Required]
+        public int AnoFabricacao { get; set; }
+
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
